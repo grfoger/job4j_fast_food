7caef1b78fc97a04bc5978bfeaf869d9e1af341e
grfoger <grfoger@gmail.com>
2023-02-26 13:32:56 +0300

    add Get by id Controller

--- admin/src/main/java/ru/job4j/admin/controller/AdminController.java
+++ admin/src/main/java/ru/job4j/admin/controller/AdminController.java
@@ -4,9 +4,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import ru.job4j.admin.service.AdminService;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Controller
@@ -18,6 +20,12 @@ public class AdminController {
     @GetMapping(value = {"/", "/index"})
     public String getIndex(Model model) {
         model.addAttribute("dishes", service.findAllDishes());
+        return "index";
+    }
+
+    @GetMapping("/{id}")
+    public String getOne(@PathVariable int id, Model model) {
+        model.addAttribute("dishes", List.of(service.findById(id)));
         return "index";
     }
 
