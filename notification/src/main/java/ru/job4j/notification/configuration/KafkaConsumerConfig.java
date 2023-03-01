846814d1eb772b759e83354df3cd4f90d3ba78e7
Arhip <grfoger@gmail.com>
2023-03-01 12:58:34 +0300

    add producer - consumer kafka configurations

---
+++ notification/src/main/java/ru/job4j/notification/configuration/KafkaConsumerConfig.java
@@ -0,0 +1,48 @@
+package ru.job4j.notification.configuration;
+
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.Map;
+
+import static java.util.Map.entry;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String kafkaServer;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    private String kafkaGroupId;
+
+    @Bean
+    public KafkaListenerContainerFactory<?> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<Integer, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<Integer, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+    }
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        return Map.ofEntries(
+                entry(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer),
+                entry(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class),
+                entry(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class),
+                entry(ConsumerConfig.GROUP_ID_CONFIG, kafkaGroupId)
+        );
+    }
+}
