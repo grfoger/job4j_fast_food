9d525bba17691b52b5695cd26453e84b96d631e1
grfoger <grfoger@gmail.com>
2023-03-07 07:57:24 +0300

    add base cook method

--- kitchen/src/main/java/ru/job4j/kitchen/service/SimpleOrderService.java
+++ kitchen/src/main/java/ru/job4j/kitchen/service/SimpleOrderService.java
@@ -1,6 +1,9 @@
 package ru.job4j.kitchen.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import ru.job4j.domain.model.Order;
@@ -10,15 +13,24 @@ import ru.job4j.kitchen.repository.OrderRepository;
 
 @Service
 @AllArgsConstructor
-public class SimpleOrderService implements OrderService{
-
+public class SimpleOrderService implements OrderService {
     private final OrderRepository repository;
+    private final KafkaTemplate<Integer, String> template;
     @Override
     public void save(Order value) {
         OrderDTO order = repository.save(new OrderDTO(0, value.getOrderNumber(), OrderStatus.CONFIRMED.getStatusCode()));
-        cookOrder(order);
+        cookOrder(value, order.getId());
     }
 
-    private void cookOrder(OrderDTO order) {
+    private void cookOrder(Order order, int orderId) {
+        try {
+            Thread.sleep(1000);
+        order.setStatus(OrderStatus.SENT);
+        repository.save(new OrderDTO(orderId, order.getOrderNumber(), order.getStatus().getStatusCode()));
+        template.send("status", new ObjectMapper().writeValueAsString(order));
+        } catch (InterruptedException | JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
     }
 }
