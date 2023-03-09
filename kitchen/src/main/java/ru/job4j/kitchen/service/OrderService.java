4651a4eb248c83ccd065a05ae9c194abb8e18ff7
grfoger <grfoger@gmail.com>
2023-03-09 19:51:39 +0300

    correct cook method

--- kitchen/src/main/java/ru/job4j/kitchen/service/OrderService.java
+++ kitchen/src/main/java/ru/job4j/kitchen/service/OrderService.java
@@ -1,9 +1,48 @@
 package ru.job4j.kitchen.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import ru.job4j.domain.model.Order;
+import ru.job4j.domain.model.OrderStatus;
+import ru.job4j.kitchen.model.OrderDTO;
+import ru.job4j.kitchen.repository.OrderRepository;
+
+
 
 @Service
-public interface OrderService {
-    void save(Order value);
+@AllArgsConstructor
+@EnableScheduling
+public class OrderService {
+    private final OrderRepository repository;
+    private final KafkaTemplate<Integer, String> template;
+
+    public void save(Order value) {
+        OrderDTO order = repository.save(new OrderDTO(0, value.getOrderNumber(), OrderStatus.CONFIRMED.getStatusCode()));
+        cookOrder(value, order.getId());
+    }
+
+    private void cookOrder(Order order, int orderId) {
+        try {
+        if ((int) System.currentTimeMillis() % 10 != 1) {
+                itsCookTime();
+                order.setStatus(OrderStatus.SENT);
+                repository.save(new OrderDTO(orderId, order.getOrderNumber(), order.getStatus().getStatusCode()));
+                template.send("cooked_order", new ObjectMapper().writeValueAsString(OrderStatus.DELIVERED));
+        } else {
+            template.send("cooked_order", new ObjectMapper().writeValueAsString(OrderStatus.CANCELED));
+        }
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Scheduled(fixedDelay = 60_000)
+    public void itsCookTime() {
+        System.out.println("It's cook time!");
+    }
 }
--- order/src/main/java/ru/job4j/order/service/OrderService.java
+++ order/src/main/java/ru/job4j/order/service/OrderService.java
@@ -1,19 +1,72 @@
 package ru.job4j.order.service;
 
-import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 import ru.job4j.domain.model.Order;
 import ru.job4j.domain.model.OrderStatus;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public interface OrderService {
-    Order save(Order order);
-    Order delete(Order order);
-    Optional<Order> findById(int id);
-    List<Order> findAll();
 
-    OrderStatus getStatus();
+@Service
+public class OrderService {
+
+    private KafkaTemplate<Integer, String> template;
+
+    private List<Order> takeOrders;
+
+    private OrderStatus currentStatus = OrderStatus.NOT_AVAILABLE;
+
+    public OrderService(KafkaTemplate<Integer, String> template) {
+        this.template = template;
+    }
+
+
+    public Order save(Order order) {
+        try {
+            template.send("orders", new ObjectMapper().writeValueAsString(order));
+            template.send("preorder", new ObjectMapper().writeValueAsString(order));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("SENT");
+        return order;
+    }
+
+
+
+
+    public List<Order> findAll() {
+        template.send("giveMe", "all");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return takeOrders;
+    }
+
+
+
+    public OrderStatus getStatus() {
+        return currentStatus;
+    }
+    @KafkaListener(topics = {"cooked_order"})
+    public void getKafkaStatus(ConsumerRecord<Integer, String> status) throws IOException {
+        currentStatus = new ObjectMapper().readValue(status.value().getBytes(), OrderStatus.class);
+    }
+
+
+    @KafkaListener(topics = {"findAll"})
+    public void takeMessage(ConsumerRecord<Integer, String> orders) throws IOException {
+        takeOrders = new ObjectMapper().readValue(orders.value(), new TypeReference<>() {
+        });
+    }
 }
